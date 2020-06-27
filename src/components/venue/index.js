986f9712703fd24fd2621d53bdaf4fe798e7327a
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-06-27 17:32:06 -0400

    finished Venue component

--- src/components/venue/index.js
+++ src/components/venue/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Zoom from 'react-reveal/Zoom';
 import calendar_icon from '../../resources/images/icons/calendar.png';
 import location_icon from '../../resources/images/icons/location.png';
 
@@ -7,6 +8,42 @@ const Venue = () => {
     <div className='bck_black'>
       <div className='center_wrapper'>
         <div className='vn_wrapper'>
+          
+          <Zoom duration={500}>
+            <div className='vn_item'>
+              <div className='vn_outer'>
+                <div className='vn_inner'> 
+                  <div className='vn_icon_square bck_pink'></div>
+                  <div className='vn_icon'
+                    style={{
+                      background: `url(${calendar_icon})`
+                    }}
+                  >
+                  </div>
+                  <div className='vn_title'>Event Date & Time</div>
+                  <div className='vn_desc'>Aug 19 2020 9:00pm</div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+
+         <Zoom duration={500} delay={500}>
+            <div className='vn_item'>
+              <div className='vn_outer'>
+                <div className='vn_inner'> 
+                  <div className='vn_icon_square bck_lpink'></div>
+                  <div className='vn_icon'
+                    style={{
+                      background: `url(${location_icon})`
+                    }}
+                  >
+                  </div>
+                  <div className='vn_title'>Location</div>
+                  <div className='vn_desc'>Auditorio Nacional Cd de México</div>
+                </div>
+              </div>
+            </div>
+         </Zoom>
 
         </div>
       </div>
