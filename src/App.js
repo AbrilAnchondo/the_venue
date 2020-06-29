fdc0b8676234f5ea8c4d201dd3055a3b19c9bd1b
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-06-29 00:49:37 -0400

    added scroll functionality with react scroll library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './resources/styles.css';
+import { Element } from 'react-scroll';
+
 import Header from './components/header_and_footer/Header';
 import Footer from './components/header_and_footer/Footer';
 import Featured from './components/featured';
@@ -13,11 +15,27 @@ function App() {
   return (
     <div style={{background: 'white'}}>
      <Header />
-     <Featured />
-     <Venue />
-     <Highlights />
-     <Pricing />
-     <Location />
+
+     <Element name='featured'>
+      <Featured />
+    </Element>
+
+    <Element name='venue'>
+      <Venue />
+    </Element>
+
+     <Element name='highlights'>
+      <Highlights />
+     </Element>
+
+    <Element name='pricing'>
+      <Pricing />
+    </Element>
+
+    <Element name='location'>
+      <Location />
+    </Element>
+
      <Footer />
     </div>
   );
--- src/components/header_and_footer/SideDrawer.js
+++ src/components/header_and_footer/SideDrawer.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { scroller } from 'react-scroll';
+
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = (props) => {
+
+  const scrollToSection = (section) => {
+    scroller.scrollTo(section,{
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: -150
+    });
+    props.onClose(false)
+  }
+
   return (
     <Drawer
       anchor='right'
@@ -11,19 +24,19 @@ const SideDrawer = (props) => {
       onClose={() => props.onClose(false)}
     >
       <List component='nav'>
-        <ListItem button onClick={() => console.log('clicked menu item')}>
+        <ListItem button onClick={() => scrollToSection('featured')}>
           Date
         </ListItem>
-        <ListItem button onClick={() => console.log('clicked menu item')}>
+        <ListItem button onClick={() => scrollToSection('venue')}>
           Venue
         </ListItem>
-        <ListItem button onClick={() => console.log('clicked menu item')}>
+        <ListItem button onClick={() => scrollToSection('highlights')}>
           Highlights
         </ListItem>
-        <ListItem button onClick={() => console.log('clicked menu item')}>
+        <ListItem button onClick={() => scrollToSection('pricing')}>
           Pricing
         </ListItem>
-        <ListItem button onClick={() => console.log('clicked menu item')}>
+        <ListItem button onClick={() => scrollToSection('location')}>
           Location
         </ListItem>
       </List>
